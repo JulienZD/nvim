1991ffa8d9afbbbe1e864800c81ba05a04ee77ab
Julien Zapata Duque <jzapata@live.nl>
2024-10-09 08:00:08 +0200

    feat: add neo-tree, autopairs, and neoscroll

--- init.lua
+++ init.lua
@@ -930,8 +930,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -944,6 +944,52 @@ require('lazy').setup({
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
+  },
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      local neoscroll = require 'neoscroll'
+
+      local keymap = {
+        ['<C-u>'] = function()
+          neoscroll.ctrl_u { duration = 150 }
+        end,
+        ['<C-d>'] = function()
+          neoscroll.ctrl_d { duration = 150 }
+        end,
+        ['<C-b>'] = function()
+          neoscroll.ctrl_b { duration = 450 }
+        end,
+        ['<C-f>'] = function()
+          neoscroll.ctrl_f { duration = 450 }
+        end,
+        ['<C-y>'] = function()
+          neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+        end,
+        ['<C-e>'] = function()
+          neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+        end,
+        ['zt'] = function()
+          neoscroll.zt { half_win_duration = 250 }
+        end,
+        ['zz'] = function()
+          neoscroll.zz { half_win_duration = 250 }
+        end,
+        ['zb'] = function()
+          neoscroll.zb { half_win_duration = 250 }
+        end,
+      }
+      local modes = { 'n', 'v', 'x' }
+      for key, func in pairs(keymap) do
+        vim.keymap.set(modes, key, func)
+      end
+    end,
+  },
+  {
+    'axelvc/template-string.nvim',
+    opts = {
+      remove_template_string = true,
+    },
   },
 }, {
   ui = {
