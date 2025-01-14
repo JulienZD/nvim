9fa4024bfe67f4042ec58b42a621372e0a7bba06
Julien Zapata Duque <jzapata@live.nl>
2025-01-14 08:47:39 +0100

    feat: add no-neck-pain.nvim

---
+++ lua/custom/plugins/no-neck-pain.lua
@@ -0,0 +1,15 @@
+return {
+  'shortcuts/no-neck-pain.nvim',
+  version = '*',
+  opts = {
+    buffers = {
+      right = {
+        enabled = false,
+      },
+    },
+    mappings = {
+      enabled = true,
+      toggle = '<Leader>np',
+    },
+  },
+}
