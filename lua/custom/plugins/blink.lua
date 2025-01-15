4c59f47d088edcd839ea9e4869b5b5a24a0bcca0
Julien Zapata Duque <jzapata@live.nl>
2025-01-15 09:13:29 +0100

    refactor: move blink to separate file
    There's one config option that's breaking automatching pairs, as there's an
    opening bracket inside a string, this made it impossible to easily navigate
    around my config

---
+++ lua/custom/plugins/blink.lua
@@ -0,0 +1,113 @@
+return {
+  'saghen/blink.cmp',
+  lazy = false, -- lazy loading handled internally
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    {
+      'L3MON4D3/LuaSnip',
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip').filetype_extend('javascript', { 'jsdoc' })
+            require('luasnip').filetype_extend('typescript', { 'tsdoc' })
+
+            require('luasnip.loaders.from_vscode').lazy_load()
+            require('luasnip.loaders.from_lua').lazy_load {
+              paths = {
+                './lua/snippets',
+              },
+            }
+          end,
+        },
+      },
+    },
+  },
+  version = 'v0.*',
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = 'super-tab',
+      -- Also allow enter to be used to accept
+      ['<CR>'] = { 'accept', 'fallback' },
+    },
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono',
+    },
+    snippets = {
+      expand = function(snippet)
+        require('luasnip').lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require('luasnip').jumpable(filter.direction)
+        end
+        return require('luasnip').in_snippet()
+      end,
+      jump = function(direction)
+        require('luasnip').jump(direction)
+      end,
+    },
+    fuzzy = {
+      sorts = {
+        'score',
+        'sort_text',
+        -- Custom sorter to promote our own snippets to the very top, as blink demotes snippets by default
+        ---@param a blink.cmp.CompletionItem
+        ---@param b blink.cmp.CompletionItem
+        function(a, b)
+          local OWN_SNIPPET_MARKER = '__CUSTOM_SNIPPET__'
+
+          for _, item in ipairs { a, b } do
+            if item.kind == require('blink.cmp.types').CompletionItemKind.Snippet then
+              ---@type string
+              ---@diagnostic disable-next-line: assign-type-mismatch
+              local documentation = item.documentation and item.documentation.value or item.documentation or ''
+
+              if documentation:find(OWN_SNIPPET_MARKER) then
+                return true
+              end
+            end
+          end
+        end,
+      },
+    },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'luasnip', 'buffer' },
+    },
+    completion = {
+      menu = {
+        auto_show = function(ctx)
+          local is_cmdline = ctx.mode == 'cmdline'
+          local is_search = vim.tbl_contains({ '/', '?' }, vim.fn.getcmdtype())
+
+          return not is_cmdline and not is_search
+        end,
+      },
+      accept = { auto_brackets = { enabled = true } },
+      trigger = {
+        -- Don't show completion menu when typing these characters
+        show_on_blocked_trigger_characters = { ' ', '\n', '\t', "'", '"', '`', '/', '-', '(', '{', '[', ',' },
+      },
+      documentation = {
+        auto_show = true,
+      },
+    },
+  },
+  experimental = { signature = true },
+  opts_extend = { 'sources.default' },
+}
