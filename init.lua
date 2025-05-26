3cdbf6a74e321d3b7e37c3f09690b33c376d5328
Julien Zapata Duque <jzapata@live.nl>
2025-05-26 09:04:14 +0200

    feat: add keymap to show diagnostic hover

--- init.lua
+++ init.lua
@@ -96,6 +96,8 @@ end)
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<leader>dh', vim.diagnostic.open_float, { desc = 'Show [D]iagnostic [H]over' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
