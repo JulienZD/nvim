f18ab4ff254d05e80f88edfddbaf2fe4153d9aed
Julien Zapata Duque <jzapata@live.nl>
2025-07-08 11:12:14 +0200

    fix(oil): don't hide files starting with .git, only exactly .git

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
     view_options = {
       show_hidden = true,
       is_always_hidden = function(name)
-        local always_hidden_patterns = { '^%.git', '^%.cache', '.DS_Store', '../' }
+        local always_hidden_patterns = { '^%.git$', '^%.cache', '.DS_Store', '../' }
 
         for _, pattern in ipairs(always_hidden_patterns) do
           if name:match(pattern) then
