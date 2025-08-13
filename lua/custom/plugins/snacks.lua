33fb83290074968eb693b709b8b165467cf78454
Julien Zapata Duque <jzapata@live.nl>
2025-08-13 08:47:57 +0200

    feat: add jiratime toggle

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -22,6 +22,9 @@ return {
           },
         },
       },
+      terminal = {
+        enabled = true,
+      },
       dashboard = {
         enabled = false,
         sections = {
@@ -72,6 +75,43 @@ return {
           Snacks.scratch()
         end,
         desc = 'Toggle Scratch Buffer',
+      },
+      {
+        -- Note: this requires `jiratime` to be on the PATH
+        -- https://github.com/RobinHeidenis/jiratime
+        '<leader>jt',
+        (function()
+          local has_quit_attached = false
+
+          return function()
+            require('snacks.terminal').toggle 'jiratime'
+
+            if has_quit_attached then
+              return
+            end
+
+            -- Hook into the terminal buffer to hide the TUI when pressing 'q', so we can re-open it without losing state
+
+            local terminals = require('snacks.terminal').list()
+
+            local jiratime_terminal = vim.tbl_filter(function(terminal)
+              return terminal.cmd == 'jiratime'
+            end, terminals)[1]
+
+            if not jiratime_terminal then
+              return
+            end
+
+            vim.api.nvim_buf_set_keymap(jiratime_terminal.buf, 't', 'q', '<C-\\><C-n>:lua require("snacks.terminal").toggle(\'jiratime\')<CR>', {
+              noremap = true,
+              silent = true,
+              desc = 'Hide TUI Terminal',
+            })
+
+            has_quit_attached = true
+          end
+        end)(),
+        desc = 'Toggle [J]ira[T]ime',
       },
     },
   },
