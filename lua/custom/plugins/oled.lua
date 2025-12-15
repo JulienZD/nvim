77de3185857e6f0d571e3e69c546926acf075bae
Julien Zapata Duque <jzapata@live.nl>
2025-12-15 07:43:33 +0100

    feat: detect oled monitor manually, as the env doesn't always reload properly

--- lua/custom/plugins/oled.lua
+++ lua/custom/plugins/oled.lua
@@ -1,4 +1,6 @@
-local is_oled_screen = vim.env.OLED_SCREEN == '1' or vim.env.OLED_SCREEN == 'true'
+-- The following is also set in the environment, but it doesn't reload properly when detaching the monitor and
+-- restarting nvim
+local is_oled_screen = vim.fn.system 'system_profiler SPDisplaysDataType | grep -q "MO27Q28G"' == 0
 
 local M = {}
 
@@ -12,7 +14,21 @@ vim.schedule(function()
   vim.opt.fillchars:append 'vert: '
 
   -- Remove the virtual hard wrap column, as it is static content
-  require('virt-column').update { enabled = false }
+  local virt_column_shown = false
+
+  local virt_column = require 'virt-column'
+  virt_column.update { enabled = virt_column_shown }
+
+  -- Register a keymap to toggle the virt-column plugin
+  vim.keymap.set('n', '<leader>tc', function()
+    virt_column_shown = not virt_column_shown
+    virt_column.update { enabled = virt_column_shown }
+    vim.cmd 'redraw!' -- redraw to ensure the change is visible immediately
+  end, {
+    desc = '[T]oggle Virtual [C]olumn',
+    noremap = true,
+    silent = true,
+  })
 
   -- Disable statusline, as it is static content
   vim.g.ministatusline_disable = true
