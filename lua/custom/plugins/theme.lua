b909d8ffd8091ed8591f9a0addc56c65f6ee82c0
Julien Zapata Duque <jzapata@live.nl>
2026-02-10 07:47:55 +0100

    feat(theme): better oled friendly theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -9,8 +9,29 @@ return {
     -- All colorschemes: `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Load before all other start plugins
+    -- init = function()
+    --   vim.cmd.colorscheme 'tokyonight-storm'
+    --
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
+  },
+  {
+    'bluz71/vim-moonfly-colors',
+    name = 'moonfly',
+    lazy = false,
+    priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'tokyonight-storm'
+      -- Hide ~ on empty lines, otherwise they're shown all along the left side of buffers
+      vim.opt.fillchars = { eob = ' ' }
+
+      -- FIXME: This should only apply when nvim is in dark mode, as it breaks the moonfly light mode theme
+      require('moonfly').custom_colors {
+        bg = '#000000',
+        grey15 = '#000000', -- dropbar.nvim background
+        grey16 = '#080808', -- splits background
+      }
+
+      vim.cmd.colorscheme 'moonfly'
 
       vim.cmd.hi 'Comment gui=none'
     end,
