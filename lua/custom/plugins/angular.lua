e36482cae9d4b015679690ddb49436557e594ec0
Julien Zapata Duque <jzapata@live.nl>
2026-02-17 15:01:11 +0100

    feat(treesitter): use latest version for angular fixes

--- lua/custom/plugins/angular.lua
+++ lua/custom/plugins/angular.lua
@@ -4,19 +4,6 @@ vim.filetype.add {
   },
 }
 
--- Temporary override containing the fix from https://github.com/dlvandenberg/tree-sitter-angular/pull/85
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'TSUpdate',
-  callback = function()
-    require('nvim-treesitter.parsers').angular = {
-      install_info = {
-        url = 'https://github.com/JulienZD/tree-sitter-angular',
-      },
-      filetype = 'htmlangular',
-    }
-  end,
-})
-
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'htmlangular',
   callback = function()
